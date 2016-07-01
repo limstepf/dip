ae9a41ae841d9f8258456a19efb4b3bc646246ac
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-01 05:05:30 +0200

    Implement/extend InvertOp by ranged sample inversion

--- api/src/main/java/ch/unifr/diva/dip/api/imaging/ops/InvertOp.java
+++ api/src/main/java/ch/unifr/diva/dip/api/imaging/ops/InvertOp.java
@@ -5,17 +5,56 @@ import ch.unifr.diva.dip.api.imaging.scanners.Location;
 import ch.unifr.diva.dip.api.imaging.scanners.RasterScanner;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.util.Arrays;
 
 /**
- * Image inversion filter.
+ * Image inversion filter. Inverts samples based on the sample domain, or a
+ * custom sample range.
  *
  * <pre>I_inverted(x,y) = M - I(x,y)</pre>, where
  * <pre>M</pre> is the maximum value (e.g. 255 for an 8-bit sample).
  */
 public class InvertOp extends NullOp {
 
-	// TODO: make this work with any SimpleColorModel's and respect
-	// the defined ranges. Then again, not sure how usefull this is...
+	// given range [a, b] then x' = b - x + a, so the rangeOffset is a + b, from
+	// which we can substract x to get x'
+	private double[] rangeOffset;
+
+	/**
+	 * Creates a default InvertOp {@literal w.r.t.} the whole sample domain. Can
+	 * handle bit and byte samples only.
+	 */
+	public InvertOp() {
+		this.rangeOffset = null;
+	}
+
+	/**
+	 * Creates an InvertOp {@literal  w.r.t.} a specified range. Can handle
+	 * samples in any precision.
+	 *
+	 * @param min minimum value(s)
+	 * @param max maximum value(s)
+	 */
+	public InvertOp(double[] min, double[] max) {
+		this.rangeOffset = new double[min.length];
+
+		for (int i = 0; i < min.length; i++) {
+			this.rangeOffset[i] = min[i] + max[i];
+		}
+	}
+
+	private boolean hasRange() {
+		return (this.rangeOffset != null);
+	}
+
+	// returns value specified for the band, or the last one defined
+	private double get(int index, double[] values) {
+		if (index < values.length) {
+			return values[index];
+		}
+
+		return values[values.length - 1];
+	}
 
 	@Override
 	public BufferedImage filter(BufferedImage src, BufferedImage dst) {
@@ -26,13 +65,27 @@ public class InvertOp extends NullOp {
 		final WritableRaster srcRaster = src.getRaster();
 		final WritableRaster dstRaster = dst.getRaster();
 
-		final int maxValue = ImagingUtils.maxSampleValue(src);
+		if (hasRange()) {
+			for (Location pt : new RasterScanner(src, true)) {
+				final int sample = srcRaster.getSample(pt.col, pt.row, pt.band);
+				dstRaster.setSample(
+						pt.col, pt.row, pt.band,
+						get(pt.band, this.rangeOffset) - sample
+				);
+			}
+		} else {
+			final int maxValue = ImagingUtils.maxSampleValue(src);
 
-		for (Location pt : new RasterScanner(src, true)) {
-			final int sample = srcRaster.getSample(pt.col, pt.row, pt.band);
-			dstRaster.setSample(pt.col, pt.row, pt.band, maxValue - sample);
+			for (Location pt : new RasterScanner(src, true)) {
+				final int sample = srcRaster.getSample(pt.col, pt.row, pt.band);
+				dstRaster.setSample(
+						pt.col, pt.row, pt.band,
+						maxValue - sample
+				);
+			}
 		}
 
 		return dst;
 	}
+
 }
