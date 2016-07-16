9af3831a28d7616ffd58ffa160afac749dc5cf94
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-16 15:51:15 +0200

    Add ConvolutionOp and ConvolutionFilter

---
+++ api/src/main/java/ch/unifr/diva/dip/api/imaging/ops/ConvolutionOp.java
@@ -0,0 +1,282 @@
+package ch.unifr.diva.dip.api.imaging.ops;
+
+import ch.unifr.diva.dip.api.datastructures.DoubleMatrix;
+import ch.unifr.diva.dip.api.datastructures.KernelBase;
+import ch.unifr.diva.dip.api.datastructures.MatrixBase;
+import ch.unifr.diva.dip.api.imaging.ImagingUtils;
+import ch.unifr.diva.dip.api.imaging.padders.ImagePadder;
+import ch.unifr.diva.dip.api.imaging.scanners.Location;
+import ch.unifr.diva.dip.api.imaging.scanners.PaddedImageTiler;
+import ch.unifr.diva.dip.api.imaging.scanners.RasterScanner;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+/**
+ * Convolution filter.
+ *
+ * @param <T> class of the matrix backing the used kernel.
+ */
+public class ConvolutionOp<T extends MatrixBase> extends NullOp implements PaddedParallelizable {
+
+	private final ImagePadder padder;
+	private final KernelBase<T> kernel; // *the* kernel, or just the row vector if separable
+	private final KernelBase<T> columnVector; // ...or, null if not separable
+	private final ConvolutionOp[] convolutionPasses; // 1st and 2nd pass, null if not separable
+	private final boolean isDoublePrecision;
+	private final boolean[] abs;
+	private final double[] gain;
+	private final double[] bias;
+	private final double[] min;
+	private final double[] max;
+	private final SamplePrecision precision;
+
+	/**
+	 * Creates the single-pass convolution filter for a double-pass convolution
+	 * with a separable kernel.
+	 *
+	 * @param kernel the row or column vector.
+	 * @param op the parent double-pass convolution filter.
+	 */
+	private ConvolutionOp(KernelBase<T> kernel, ConvolutionOp op) {
+		this.padder = op.padder;
+		this.kernel = kernel;
+		this.isDoublePrecision = this.kernel.matrix() instanceof DoubleMatrix;
+		this.columnVector = null;
+		this.convolutionPasses = null;
+
+		this.abs = op.abs;
+		this.gain = op.gain;
+		this.bias = op.bias;
+		this.min = op.min;
+		this.max = op.max;
+		this.precision = op.precision;
+	}
+
+	/**
+	 * Creates a new convolution filter.
+	 *
+	 * @param rowVector the kernel (single-pass convolution), or the row vector
+	 * for double-pass convolution with a separable kernel.
+	 * @param columnVector null (single-pass convolution), or the column vector
+	 * for double-pass convolution with a separable kernel.
+	 * @param padder an image padder for edge handling.
+	 * @param abs take the absolute value of the sample first (per band). Can be
+	 * null.
+	 * @param gain gain per band.
+	 * @param bias bias per band.
+	 * @param min minimum value per band used for clamping.
+	 * @param max maximum value per band used for clamping.
+	 * @param precision desired output sample precision.
+	 */
+	public ConvolutionOp(KernelBase<T> rowVector, KernelBase<T> columnVector, ImagePadder padder,
+			boolean[] abs, double[] gain, double[] bias, double[] min, double[] max, SamplePrecision precision) {
+		this.padder = padder;
+		this.kernel = rowVector;
+		this.isDoublePrecision = this.kernel.matrix() instanceof DoubleMatrix;
+
+		this.columnVector = columnVector;
+		if (this.columnVector == null) {
+			this.convolutionPasses = null;
+		} else {
+			this.convolutionPasses = new ConvolutionOp[2];
+			this.convolutionPasses[0] = new ConvolutionOp(this.columnVector, this);
+			this.convolutionPasses[1] = new ConvolutionOp(this.kernel, this);
+		}
+
+		this.abs = abs;
+		this.gain = gain;
+		this.bias = bias;
+		this.min = min;
+		this.max = max;
+		this.precision = precision;
+	}
+
+	private float convolveAtFloat(BufferedImage src, Location pt) {
+		float sum = 0;
+
+		for (Location kernelPoint : new RasterScanner(kernel.bounds())) {
+			sum += this.kernel.getValueFloat(kernelPoint.col, kernelPoint.row)
+					* this.padder.getSampleFloat(
+							src,
+							pt.col - kernelPoint.col,
+							pt.row - kernelPoint.row,
+							pt.band
+					);
+		}
+
+		return sum;
+	}
+
+	private double convolveAtDouble(BufferedImage src, Location pt) {
+		double sum = 0;
+
+		for (Location kernelPoint : new RasterScanner(kernel.bounds())) {
+			sum += this.kernel.getValueDouble(kernelPoint.col, kernelPoint.row)
+					* this.padder.getSampleDouble(
+							src,
+							pt.col - kernelPoint.col,
+							pt.row - kernelPoint.row,
+							pt.band
+					);
+		}
+
+		return sum;
+	}
+
+	// check whether we do rescaling at all
+	private boolean doRescale() {
+		if (this.min == null || this.max == null || this.gain == null || this.bias == null) {
+			return false;
+		}
+		return true;
+	}
+
+	// check whether at least one band need to take the absolute sample value
+	private boolean takeAbsValue() {
+		if (this.abs == null) {
+			return false;
+		}
+		for (int i = 0; i < abs.length; i++) {
+			if (abs[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// return absolute sample value, or not, depending on spec.
+	private double abs(int band, double value) {
+		return abs[band] ? Math.abs(value) : value;
+	}
+
+	@Override
+	public PaddedImageTiler getImageTiler(BufferedImage src, int width, int height) {
+		return new PaddedImageTiler(
+				src,
+				width,
+				height,
+				this.kernel.width() / 2,
+				this.kernel.height() / 2
+		);
+	}
+
+	@Override
+	public BufferedImage filter(BufferedImage src, BufferedImage dst) {
+		return filter(src, dst, null);
+	}
+
+	@Override
+	public BufferedImage filter(BufferedImage src, BufferedImage dst, Rectangle writableRegion) {
+		if (dst == null) {
+			dst = this.createCompatibleDestImage(src, this.precision);
+		}
+
+		// separable double-pass convolution?
+		if (this.columnVector != null) {
+			return this.convolutionPasses[1].filter(
+					this.convolutionPasses[0].filter(src, null, writableRegion),
+					dst,
+					writableRegion
+			);
+		}
+
+		// non-separable/single-pass convolution
+		final WritableRaster dstRaster = dst.getRaster();
+		final WritableRaster raster = getRasterWithLeastBands(src.getRaster(), dstRaster);
+
+		// things get a bit messy here to not have a thousand conditionals
+		// withing the main loop, so:
+		// 1) we're either working with a double, or a float matrix
+		// 2) we do rescaling (gain * I(x,y) + bias), and clamping; or not
+		// 3) we do take the absolute value (of some bands), or not at all
+		if (this.isDoublePrecision) {
+			// DOUBLE PRECISION
+			if (!doRescale()) {
+				if (takeAbsValue()) {
+					// double | abs
+					for (Location pt : new RasterScanner(raster)) {
+						dstRaster.setSample(pt.col, pt.row, pt.band,
+								abs(pt.band, convolveAtDouble(src, pt))
+						);
+					}
+				} else {
+					// double
+					for (Location pt : new RasterScanner(raster)) {
+						dstRaster.setSample(pt.col, pt.row, pt.band,
+								convolveAtDouble(src, pt)
+						);
+					}
+				}
+			} else if (takeAbsValue()) {
+				// double | abs | rescaling
+				for (Location pt : new RasterScanner(raster)) {
+					dstRaster.setSample(pt.col, pt.row, pt.band,
+							ImagingUtils.clamp(
+									gain[pt.band] * abs(pt.band, convolveAtDouble(src, pt)) + bias[pt.band],
+									min[pt.band],
+									max[pt.band]
+							)
+					);
+				}
+			} else {
+				// double | rescaling
+				for (Location pt : new RasterScanner(raster)) {
+					dstRaster.setSample(pt.col, pt.row, pt.band,
+							ImagingUtils.clamp(
+									gain[pt.band] * convolveAtDouble(src, pt) + bias[pt.band],
+									min[pt.band],
+									max[pt.band]
+							)
+					);
+				}
+			}
+
+		} else {
+			// FLOAT PRECISION
+			if (!doRescale()) {
+				// float | abs
+				if (takeAbsValue()) {
+					for (Location pt : new RasterScanner(raster)) {
+						dstRaster.setSample(pt.col, pt.row, pt.band,
+								abs(pt.band, convolveAtFloat(src, pt))
+						);
+					}
+				} else {
+					// float
+					for (Location pt : new RasterScanner(raster)) {
+						dstRaster.setSample(pt.col, pt.row, pt.band,
+								convolveAtFloat(src, pt)
+						);
+					}
+				}
+			} else if (takeAbsValue()) {
+				// float | abs | rescaling
+				for (Location pt : new RasterScanner(raster)) {
+					dstRaster.setSample(pt.col, pt.row, pt.band,
+							ImagingUtils.clamp(
+									gain[pt.band] * abs(pt.band, convolveAtFloat(src, pt)) + bias[pt.band],
+									min[pt.band],
+									max[pt.band]
+							)
+					);
+				}
+			} else {
+				// float | rescaling
+				for (Location pt : new RasterScanner(raster)) {
+					dstRaster.setSample(pt.col, pt.row, pt.band,
+							ImagingUtils.clamp(
+									gain[pt.band] * convolveAtFloat(src, pt) + bias[pt.band],
+									min[pt.band],
+									max[pt.band]
+							)
+					);
+				}
+			}
+
+		}
+
+		return dst;
+	}
+
+}
---
+++ awt-tools/src/main/java/ch/unifr/diva/dip/awt/tools/ConvolutionFilter.java
@@ -0,0 +1,401 @@
+package ch.unifr.diva.dip.awt.tools;
+
+import ch.unifr.diva.dip.api.components.InputPort;
+import ch.unifr.diva.dip.api.components.OutputPort;
+import ch.unifr.diva.dip.api.components.Port;
+import ch.unifr.diva.dip.api.components.ProcessorContext;
+import ch.unifr.diva.dip.api.datastructures.DoubleKernel;
+import ch.unifr.diva.dip.api.datastructures.DoubleMatrix;
+import ch.unifr.diva.dip.api.datastructures.FloatKernel;
+import ch.unifr.diva.dip.api.datastructures.FloatMatrix;
+import ch.unifr.diva.dip.api.datastructures.KernelBase;
+import ch.unifr.diva.dip.api.datastructures.MatrixBase;
+import ch.unifr.diva.dip.api.imaging.BufferedMatrix;
+import ch.unifr.diva.dip.api.imaging.ops.ConvolutionOp;
+import ch.unifr.diva.dip.api.imaging.padders.ImagePadder;
+import ch.unifr.diva.dip.api.parameters.CompositeGrid;
+import ch.unifr.diva.dip.api.parameters.EnumParameter;
+import ch.unifr.diva.dip.api.parameters.LabelParameter;
+import ch.unifr.diva.dip.api.services.ProcessableBase;
+import ch.unifr.diva.dip.api.services.Processor;
+import ch.unifr.diva.dip.api.services.Transmutable;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+
+/**
+ * A convolution filter.
+ */
+@Component
+@Service
+public class ConvolutionFilter extends ProcessableBase implements Transmutable {
+
+	private final static String IMAGE_FORMAT = "PNG";
+	private final static String IMAGE_FILE = "convolved.png";
+	private final static String MATRIX_FILE = "convolved.bmat";
+
+	private final InputPort<BufferedImage> input;
+	private final InputPort<BufferedImage> input_float;
+
+	private final KernelInput<FloatMatrix> kernelFloat;
+	private final KernelInput<DoubleMatrix> kernelDouble;
+
+	private final RescaleUnit rescaleUnit;
+	private final CompositeGrid configGrid;
+	private final EnumParameter kernelPrecision;
+	private final EnumParameter padderOption;
+
+	/**
+	 * Kernel precision.
+	 */
+	private enum KernelPrecision {
+
+		FLOAT,
+		DOUBLE;
+
+		public static KernelPrecision get(String name) {
+			if (DOUBLE.name().equals(name)) {
+				return DOUBLE;
+			}
+			return FLOAT;
+		}
+	}
+
+	/**
+	 * Kernel input set.
+	 *
+	 * @param <T>
+	 */
+	private static class KernelInput<T extends MatrixBase> {
+
+		private final String postfix;
+		public final InputPort<T> kernel;
+		public final InputPort<T> rowVector;
+		public final InputPort<T> columnVector;
+
+		public KernelInput(Class<T> clazz) {
+			if (clazz.equals(DoubleMatrix.class)) {
+				this.postfix = "d";
+				this.kernel = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.DoubleMatrix(), false);
+				this.rowVector = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.DoubleMatrix(), false);
+				this.columnVector = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.DoubleMatrix(), false);
+			} else {
+				this.postfix = "f";
+				this.kernel = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.FloatMatrix(), false);
+				this.rowVector = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.FloatMatrix(), false);
+				this.columnVector = new InputPort<>(new ch.unifr.diva.dip.api.datatypes.FloatMatrix(), false);
+			}
+		}
+
+		public void registerPorts(Map<String, InputPort> map) {
+			map.put("kernel-" + this.postfix, kernel);
+			map.put("row-vector-" + this.postfix, rowVector);
+			map.put("column-vector-" + this.postfix, columnVector);
+		}
+
+		public boolean isSeparable() {
+			if (!this.rowVector.isConnected() || !this.rowVector.getPortState().equals(Port.State.READY)) {
+				return false;
+			}
+			if (!this.columnVector.isConnected() || !this.columnVector.getPortState().equals(Port.State.READY)) {
+				return false;
+			}
+			return true;
+		}
+	}
+
+	public ConvolutionFilter() {
+		super("Convolution Filter");
+
+		// parameters/config
+		this.kernelPrecision = new EnumParameter(
+				"precision", KernelPrecision.class, KernelPrecision.FLOAT.name()
+		);
+		this.padderOption = new EnumParameter(
+				"edge handling", ImagePadder.Type.class, ImagePadder.Type.REFLECTIVE.name()
+		);
+		this.rescaleUnit = new RescaleUnit();
+		this.rescaleUnit.getBandConfig().addGridPaneViewHook((gp) -> {
+			GridPane.setConstraints(gp, 0, 1, 2, 1);
+		});
+
+		final LabelParameter kpLabel = new LabelParameter("Precision:");
+		final LabelParameter ehLabel = new LabelParameter("Edge handling:");
+		ehLabel.addLabelViewHook((l) -> {
+			GridPane.setMargin(l, new Insets(10, 0, 0, 0));
+		});
+		final CompositeGrid convolutionGrid = new CompositeGrid(
+				kpLabel,
+				this.kernelPrecision,
+				ehLabel,
+				this.padderOption
+		);
+		convolutionGrid.setColumnConstraints(1);
+		this.configGrid = new CompositeGrid(
+				convolutionGrid,
+				this.rescaleUnit.getOutputConfig(),
+				this.rescaleUnit.getBandConfig()
+		);
+		this.configGrid.setColumnWidthConstraints(0.5, 0.5);
+		this.parameters.put("config", configGrid);
+
+		// inputs
+		this.kernelFloat = new KernelInput(FloatMatrix.class);
+		this.kernelDouble = new KernelInput(DoubleMatrix.class);
+
+		this.input = new InputPort(new ch.unifr.diva.dip.api.datatypes.BufferedImage(), false);
+		this.input_float = new InputPort(new ch.unifr.diva.dip.api.datatypes.BufferedMatrixFloat(), false);
+
+		enableAllInputs();
+		enableAllOutputs();
+	}
+
+	@Override
+	public Processor newInstance(ProcessorContext context) {
+		return new ConvolutionFilter();
+	}
+
+	@Override
+	public void init(ProcessorContext context) {
+		this.rescaleUnit.update();
+		enableOutputs();
+		inputCallback();
+
+		if (context != null) {
+			restoreOutputs(context);
+		}
+
+		// add listeners
+		this.input.portStateProperty().addListener(inputListener);
+		this.input_float.portStateProperty().addListener(inputListener);
+		this.kernelPrecision.property().addListener(inputListener);
+		this.rescaleUnit.getOutputConfig().property().addListener(configListener);
+	}
+
+	private final InvalidationListener configListener = (c) -> configCallback();
+
+	private void configCallback() {
+		this.rescaleUnit.update();
+		enableOutputs();
+		transmute();
+	}
+
+	private final InvalidationListener inputListener = (c) -> inputCallback();
+
+	private void inputCallback() {
+		if (this.input.isConnected()) {
+			enableInputs(this.input);
+		} else if (this.input_float.isConnected()) {
+			enableInputs(this.input_float);
+		} else {
+			enableInputs(null);
+		}
+		transmute();
+	}
+
+	private void enableInputs(InputPort input) {
+		this.inputs.clear();
+
+		enableSourceInputs(input);
+
+		final KernelPrecision p = KernelPrecision.get(this.kernelPrecision.get());
+		switch (p) {
+			case FLOAT:
+				this.kernelFloat.registerPorts(this.inputs);
+				break;
+			case DOUBLE:
+				this.kernelDouble.registerPorts(this.inputs);
+				break;
+		}
+	}
+
+	private void enableAllInputs() {
+		this.inputs.clear();
+
+		enableSourceInputs(null);
+
+		this.kernelFloat.registerPorts(this.inputs);
+		this.kernelDouble.registerPorts(this.inputs);
+	}
+
+	private void enableSourceInputs(InputPort input) {
+		if (input == null || input.equals(this.input)) {
+			this.inputs.put("buffered-image", this.input);
+		}
+		if (input == null || input.equals(this.input_float)) {
+			this.inputs.put("buffered-matrix-float", this.input_float);
+		}
+	}
+
+	private InputPort<BufferedImage> getConnectedInput() {
+		if (this.input_float.isConnected()) {
+			return this.input_float;
+		}
+		return this.input;
+	}
+
+	private void enableOutputs() {
+		this.outputs.clear();
+		this.outputs.putAll(this.rescaleUnit.getEnabledOutputPorts());
+	}
+
+	private void enableAllOutputs() {
+		this.outputs.clear();
+		this.outputs.putAll(this.rescaleUnit.getAllOutputPorts());
+	}
+
+	@Override
+	protected void resetOutputs() {
+		for (OutputPort output : this.rescaleUnit.getAllOutputPorts().values()) {
+			output.setOutput(null);
+		}
+	}
+
+	private boolean restoreOutputs(ProcessorContext context) {
+		return restoreOutputs(context, null);
+	}
+
+	private boolean restoreOutputs(ProcessorContext context, BufferedImage convolvedImage) {
+		final BufferedImage image;
+		if (convolvedImage == null) {
+			if (this.rescaleUnit.isBufferedMatrix()) {
+				image = readBufferedMatrix(context, MATRIX_FILE);
+			} else {
+				image = readBufferedImage(context, IMAGE_FILE);
+			}
+		} else {
+			image = convolvedImage;
+		}
+
+		if (image == null) {
+			return false;
+		}
+
+		for (OutputPort output : this.rescaleUnit.getEnabledOutputPorts().values()) {
+			output.setOutput(image);
+		}
+
+		if (!this.rescaleUnit.isBufferedMatrix()) {
+			provideImageLayer(context, image);
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean isConnected() {
+		// check image source
+		if (!getConnectedInput().isConnected()) {
+			return false;
+		}
+
+		// check kernel (non-sep. kernel, or column and row vectors
+		final KernelPrecision p = KernelPrecision.get(this.kernelPrecision.get());
+		switch (p) {
+			case FLOAT:
+				if (!(this.kernelFloat.kernel.isConnected()
+						|| (this.kernelFloat.columnVector.isConnected()
+						&& this.kernelFloat.rowVector.isConnected()))) {
+					return false;
+				}
+				break;
+
+			case DOUBLE:
+				if (!(this.kernelDouble.kernel.isConnected()
+						|| (this.kernelDouble.columnVector.isConnected()
+						&& this.kernelDouble.rowVector.isConnected()))) {
+					return false;
+				}
+				break;
+		}
+
+		return true;
+	}
+
+	@Override
+	public void process(ProcessorContext context) {
+		if (!restoreOutputs(context)) {
+			final InputPort<BufferedImage> source = getConnectedInput();
+			final BufferedImage src = source.getValue();
+
+			final KernelBase kernel;
+			final KernelBase columnVector;
+			final KernelPrecision p = KernelPrecision.get(this.kernelPrecision.get());
+			if (p.equals(KernelPrecision.DOUBLE)) {
+				if (this.kernelDouble.isSeparable()) {
+					kernel = new DoubleKernel(this.kernelDouble.rowVector.getValue());
+					columnVector = new DoubleKernel(this.kernelDouble.columnVector.getValue());
+				} else {
+					kernel = new DoubleKernel(this.kernelDouble.kernel.getValue());
+					columnVector = null;
+				}
+			} else {
+				if (this.kernelFloat.isSeparable()) {
+					kernel = new FloatKernel(this.kernelFloat.rowVector.getValue());
+					columnVector = new FloatKernel(this.kernelFloat.columnVector.getValue());
+				} else {
+					kernel = new FloatKernel(this.kernelFloat.kernel.getValue());
+					columnVector = null;
+				}
+			}
+
+			final ImagePadder.Type padderType = EnumParameter.valueOf(
+					this.padderOption.get(),
+					ImagePadder.Type.class,
+					ImagePadder.Type.REFLECTIVE
+			);
+
+			final ConvolutionOp op = new ConvolutionOp(
+					kernel,
+					columnVector,
+					padderType.getInstance(),
+					this.rescaleUnit.getAbs(),
+					this.rescaleUnit.getGain(),
+					this.rescaleUnit.getBias(),
+					this.rescaleUnit.getMin(),
+					this.rescaleUnit.getMax(),
+					this.rescaleUnit.getPrecision()
+			);
+
+			final BufferedImage image = filter(
+					context, op, src,
+					op.createCompatibleDestImage(
+							src.getWidth(),
+							src.getHeight(),
+							this.rescaleUnit.getPrecision(),
+							this.rescaleUnit.numBands()
+					)
+			);
+
+			if (this.rescaleUnit.isBufferedMatrix()) {
+				writeBufferedMatrix(context, MATRIX_FILE, (BufferedMatrix) image);
+			} else {
+				writeBufferedImage(context, IMAGE_FILE, IMAGE_FORMAT, image);
+			}
+
+			restoreOutputs(context, image);
+		}
+	}
+
+	@Override
+	public void reset(ProcessorContext context) {
+		deleteFile(context, IMAGE_FILE);
+		deleteFile(context, MATRIX_FILE);
+		resetOutputs();
+		resetLayer(context);
+	}
+
+	private final BooleanProperty transmuteProperty = new SimpleBooleanProperty();
+
+	@Override
+	public BooleanProperty transmuteProperty() {
+		return this.transmuteProperty;
+	}
+
+}
