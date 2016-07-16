eaa1d6b526ebff9003bf52f2caacf883c41b586c
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-16 15:44:34 +0200

    Refactor RescaleOp and SampleRescaler to use RescaleUnit

--- api/src/main/java/ch/unifr/diva/dip/api/imaging/ops/RescaleOp.java
+++ api/src/main/java/ch/unifr/diva/dip/api/imaging/ops/RescaleOp.java
@@ -1,6 +1,5 @@
 package ch.unifr.diva.dip.api.imaging.ops;
 
-import ch.unifr.diva.dip.api.imaging.BufferedMatrix;
 import ch.unifr.diva.dip.api.imaging.ImagingUtils;
 import ch.unifr.diva.dip.api.imaging.scanners.Location;
 import ch.unifr.diva.dip.api.imaging.scanners.RasterScanner;
@@ -12,48 +11,30 @@ import java.awt.image.WritableRaster;
  */
 public class RescaleOp extends NullOp implements Parallelizable {
 
-	/**
-	 * Output sample precision of the produced image.
-	 */
-	public enum Precision {
-
-		/**
-		 * Bit sample precision. Single-band BufferedImage of type
-		 * TYPE_BYTE_BINARY.
-		 */
-		BIT,
-		/**
-		 * Byte sample precision. Depending on the number of bands this will be
-		 * a BufferedImage of type TYPE_BYTE_GRAY (single-band), TYPE_INT_RGB (3
-		 * bands), or TYPE_INT_ARGB (4 bands).
-		 */
-		BYTE,
-		/**
-		 * Float sample precision. BufferedMatrix of floats with the same number
-		 * of bands as the input image.
-		 */
-		FLOAT
-	}
-
+	private final boolean[] abs;
 	private final double[] gain;
 	private final double[] bias;
 	private final double[] min;
 	private final double[] max;
-	private final Precision precision;
+	private final SamplePrecision precision;
 
 	private final double[][] srcRange; // band -> [min, max]
 	private final double[][] dstRange;
 
 	/**
+	 * Gain-bias rescaling.
 	 * <pre>I'(x,y) = gain * I(x,y) + bias</pre>
 	 *
-	 * @param gain
-	 * @param bias
-	 * @param min
-	 * @param max
-	 * @param precision
+	 * @param abs take the absolute value of the sample first (per band). Can be
+	 * null.
+	 * @param gain gain per band.
+	 * @param bias bias per band.
+	 * @param min minimum value per band used for clamping.
+	 * @param max maximum value per band used for clamping.
+	 * @param precision desired output sample precision.
 	 */
-	public RescaleOp(double[] gain, double[] bias, double[] min, double[] max, Precision precision) {
+	public RescaleOp(boolean[] abs, double[] gain, double[] bias, double[] min, double[] max, SamplePrecision precision) {
+		this.abs = abs;
 		this.gain = gain;
 		this.bias = bias;
 		this.min = min;
@@ -65,13 +46,15 @@ public class RescaleOp extends NullOp implements Parallelizable {
 	}
 
 	/**
+	 * Sample domain to sample domain rescaling.
 	 * <pre>I(x,y) in [a,b] -> I'(x,y) in [a',b']</pre>
 	 *
-	 * @param srcRange
-	 * @param dstRange
-	 * @param precision
+	 * @param srcRange sample domain range of the source per band.
+	 * @param dstRange sample domain range of the destination per band.
+	 * @param precision desired output sample precision.
 	 */
-	public RescaleOp(double[][] srcRange, double[][] dstRange, Precision precision) {
+	public RescaleOp(double[][] srcRange, double[][] dstRange, SamplePrecision precision) {
+		this.abs = null;
 		this.gain = null;
 		this.bias = null;
 		this.min = null;
@@ -86,20 +69,22 @@ public class RescaleOp extends NullOp implements Parallelizable {
 		return (this.srcRange != null && this.dstRange != null);
 	}
 
-	// returns value specified for the band, or the last one defined
-	private double get(int index, double[] values) {
-		if (index < values.length) {
-			return values[index];
+	// check whether at least one band need to take the absolute sample value
+	private boolean takeAbsValue() {
+		if (this.abs == null) {
+			return false;
 		}
-
-		return values[values.length - 1];
+		for (int i = 0; i < abs.length; i++) {
+			if (abs[i]) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	private double[] get(int index, double[][] values) {
-		if (index < values.length) {
-			return values[index];
-		}
-		return values[values.length - 1];
+	// return absolute sample value, or not, depending on spec.
+	private double abs(int band, double value) {
+		return abs[band] ? Math.abs(value) : value;
 	}
 
 	@Override
@@ -110,99 +95,66 @@ public class RescaleOp extends NullOp implements Parallelizable {
 
 		final WritableRaster srcRaster = src.getRaster();
 		final WritableRaster dstRaster = dst.getRaster();
-		final int numBands = Math.min(
-				ImagingUtils.numBands(src),
-				ImagingUtils.numBands(dst)
-		);
 
 		if (rescaleByRange()) {
-			filterByRange(srcRaster, dstRaster, numBands);
+			filterByRange(srcRaster, dstRaster);
 		} else {
-			filterByGainBias(srcRaster, dstRaster, numBands);
+			filterByGainBias(srcRaster, dstRaster);
 		}
 
 		return dst;
 	}
 
-	private void filterByGainBias(WritableRaster srcRaster, WritableRaster dstRaster, int numBands) {
-		for (int band = 0; band < numBands; band++) {
-			final double gainV = get(band, this.gain);
-			final double biasV = get(band, this.bias);
-			final double minV = get(band, this.min);
-			final double maxV = get(band, this.max);
-
-			for (Location pt : new RasterScanner(srcRaster, 1)) {
+	private void filterByGainBias(WritableRaster srcRaster, WritableRaster dstRaster) {
+		if (takeAbsValue()) {
+			for (Location pt : new RasterScanner(getRasterWithLeastBands(srcRaster, dstRaster))) {
 				dstRaster.setSample(
 						pt.col,
 						pt.row,
-						band,
+						pt.band,
 						ImagingUtils.clamp(
-								gainV * srcRaster.getSampleFloat(pt.col, pt.row, band) + biasV,
-								minV,
-								maxV
+								this.gain[pt.band] * abs(pt.band, srcRaster.getSampleFloat(pt.col, pt.row, pt.band)) + this.bias[pt.band],
+								this.min[pt.band],
+								this.max[pt.band]
 						)
 				);
 			}
-		}
-	}
-
-	private void filterByRange(WritableRaster srcRaster, WritableRaster dstRaster, int numBands) {
-		for (int band = 0; band < numBands; band++) {
-			final double[] srcRangeV = get(band, this.srcRange);
-			final double[] dstRangeV = get(band, this.dstRange);
-			final double ratio = (float)((dstRangeV[1] - dstRangeV[0]) / (srcRangeV[1] - srcRangeV[0]));
-
-			for (Location pt : new RasterScanner(srcRaster, 1)) {
+		} else {
+			for (Location pt : new RasterScanner(getRasterWithLeastBands(srcRaster, dstRaster))) {
 				dstRaster.setSample(
 						pt.col,
 						pt.row,
-						band,
-						ImagingUtils.rescale(
-								srcRaster.getSampleFloat(pt.col, pt.row, band),
-								ratio,
-								srcRangeV[1],
-								dstRangeV[1]
+						pt.band,
+						ImagingUtils.clamp(
+								this.gain[pt.band] * srcRaster.getSampleFloat(pt.col, pt.row, pt.band) + this.bias[pt.band],
+								this.min[pt.band],
+								this.max[pt.band]
 						)
 				);
 			}
 		}
 	}
 
-	public BufferedImage createCompatibleDestImage(BufferedImage src, Precision precision) {
-		final int n = ImagingUtils.numBands(src);
-		switch (precision) {
-			case FLOAT:
-				return new BufferedMatrix(src.getWidth(), src.getHeight(), n);
+	private void filterByRange(WritableRaster srcRaster, WritableRaster dstRaster) {
+		final WritableRaster raster = getRasterWithLeastBands(srcRaster, dstRaster);
+		final double[] ratio = new double[raster.getNumBands()];
+		for (int i = 0; i < ratio.length; i++) {
+			ratio[i] = (this.dstRange[i][1] - this.dstRange[i][0]) / (this.srcRange[i][1] - this.srcRange[i][0]);
+		}
 
-			case BIT:
-				return new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-
-			case BYTE:
-			default:
-				return new BufferedImage(src.getWidth(), src.getHeight(), getCompatibleType(n));
+		for (Location pt : new RasterScanner(raster)) {
+			dstRaster.setSample(
+					pt.col,
+					pt.row,
+					pt.band,
+					ImagingUtils.rescale(
+							srcRaster.getSampleFloat(pt.col, pt.row, pt.band),
+							ratio[pt.band],
+							this.srcRange[pt.band][1],
+							this.dstRange[pt.band][1]
+					)
+			);
 		}
 	}
 
-	private int getCompatibleType(int numBands) {
-		switch (numBands) {
-			case 4:
-				return BufferedImage.TYPE_INT_ARGB;
-
-			case 1:
-				return BufferedImage.TYPE_BYTE_GRAY;
-
-			case 3:
-			default:
-				return BufferedImage.TYPE_INT_RGB;
-		}
-	}
-
-	/*
-	 public BufferedImage createCompatibleDestImage(BufferedImage src, SimpleColorModel cm) {
-	 if (cm.dataType().type().equals(BufferedMatrix.class)) {
-	 return new BufferedMatrix(src.getWidth(), src.getHeight(), cm.numBands());
-	 }
-	 return new BufferedImage(src.getWidth(), src.getHeight(), getCompatibleType(cm));
-	 }
-	 */
 }
