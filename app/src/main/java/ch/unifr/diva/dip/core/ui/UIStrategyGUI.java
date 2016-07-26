086a50cdcf8dba52805d3921c47de690826ac6b7
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-26 14:44:09 +0200

    Add glyph factories to UIStrategyGUI

--- app/src/main/java/ch/unifr/diva/dip/core/ui/UIStrategyGUI.java
+++ app/src/main/java/ch/unifr/diva/dip/core/ui/UIStrategyGUI.java
@@ -1,10 +1,14 @@
 package ch.unifr.diva.dip.core.ui;
 
+import ch.unifr.diva.dip.api.ui.Glyph;
+import ch.unifr.diva.dip.api.ui.NamedGlyph;
+import ch.unifr.diva.dip.glyphs.MaterialDesignIcons;
 import ch.unifr.diva.dip.gui.dialogs.ConfirmationDialog;
 import ch.unifr.diva.dip.gui.dialogs.ErrorDialog;
 import ch.unifr.diva.dip.gui.dialogs.InformationDialog;
 import ch.unifr.diva.dip.gui.dialogs.WarningDialog;
 import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.paint.Color;
 
 /**
  * UI strategy for the graphical user interface. In addition to the bare minimum
@@ -95,14 +99,14 @@ public class UIStrategyGUI implements UIStrategy {
 	 * Application wide colors. Because sometimes CSS just doesn't cut it, and
 	 * we need to to things programmatically.
 	 */
-	public static class Color {
+	public static class Colors {
 
 		/**
 		 * Color for highlighting/accenting objects. This is the
 		 * {@code -fx-accent} color as defined by the Modena style sheet we're
 		 * using as a base.
 		 */
-		public static final javafx.scene.paint.Color accent = javafx.scene.paint.Color.web("0x0096C9");
+		public static final Color accent = Color.web("0x0096C9");
 
 	}
 
@@ -125,6 +129,57 @@ public class UIStrategyGUI implements UIStrategy {
 		 * Time in milliseconds to fade in nodes.
 		 */
 		public static final int fadeInDuration = fadeOutDuration / 4;
+	}
+
+	/**
+	 * Application-wide glyphs.
+	 */
+	public static class Glyphs {
+
+		/**
+		 * Default processor glyph. Used as default if a processor doesn't have
+		 * a special glyph.
+		 */
+		public static final NamedGlyph defaultProcessor = MaterialDesignIcons.CHIP;
+	}
+
+	/**
+	 * Creates a new glyph in normal size and default color.
+	 *
+	 * @param glyph the named glyph (or glyph factory).
+	 * @return a new glyph.
+	 */
+	public static Glyph newGlyph(NamedGlyph glyph) {
+		return newGlyph(glyph, Glyph.Size.NORMAL, Colors.accent);
+	}
+
+	/**
+	 * Creates a new glyph in default color.
+	 *
+	 * @param glyph the named glyph (or glyph factory).
+	 * @param size the size of the glyph. Consider using one of the standardized
+	 * sizes defined on the {@code Size} enum. The font size specified in points
+	 * which are a real world measurement of approximately 1/72 inch.
+	 * @return a new glyph.
+	 */
+	public static Glyph newGlyph(NamedGlyph glyph, Glyph.Size size) {
+		return newGlyph(glyph, size, Colors.accent);
+	}
+
+	/**
+	 * Creates a new glyph.
+	 *
+	 * @param glyph the named glyph (or glyph factory).
+	 * @param size the size of the glyph. Consider using one of the standardized
+	 * sizes defined on the {@code Size} enum. The font size specified in points
+	 * which are a real world measurement of approximately 1/72 inch.
+	 * @param color the color of the glyph.
+	 * @return a new glyph.
+	 */
+	public static Glyph newGlyph(NamedGlyph glyph, Glyph.Size size, Color color) {
+		final Glyph g = glyph.get(size.pt);
+		g.setColor(color);
+		return g;
 	}
 
 }
