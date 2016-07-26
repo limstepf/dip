6f392054d62da932cb3fcfc72e2cb6bd13120603
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-26 14:53:21 +0200

    Add glyphs to the ProcessorsWidget

--- app/src/main/java/ch/unifr/diva/dip/gui/pe/ProcessorsWidget.java
+++ app/src/main/java/ch/unifr/diva/dip/gui/pe/ProcessorsWidget.java
@@ -1,13 +1,23 @@
 package ch.unifr.diva.dip.gui.pe;
 
 import ch.unifr.diva.dip.api.services.Processor;
+import ch.unifr.diva.dip.api.ui.Glyph;
+import ch.unifr.diva.dip.api.ui.NamedGlyph;
+import ch.unifr.diva.dip.core.model.ProcessorWrapper;
+import ch.unifr.diva.dip.core.ui.UIStrategyGUI;
 import ch.unifr.diva.dip.gui.AbstractWidget;
 import ch.unifr.diva.dip.osgi.ServiceMonitor;
+import javafx.beans.InvalidationListener;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
 import javafx.scene.input.DragEvent;
@@ -17,6 +27,7 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +42,12 @@ public class ProcessorsWidget extends AbstractWidget {
 	private final View view;
 	private final ServiceMonitor services;
 
+	/**
+	 * Creates a new processor widget.
+	 *
+	 * @param label label of the widget.
+	 * @param services the processor service monitor.
+	 */
 	public ProcessorsWidget(String label, ServiceMonitor services) {
 		super();
 
@@ -45,6 +62,9 @@ public class ProcessorsWidget extends AbstractWidget {
 		this.view.setDisable(disable);
 	}
 
+	/**
+	 * Processor list cell.
+	 */
 	public static class ProcessorListCell extends ListCell<ServiceMonitor.Service<Processor>> {
 
 		/**
@@ -53,8 +73,11 @@ public class ProcessorsWidget extends AbstractWidget {
 		public static final DataFormat OSGI_SERVICE_PROCESSOR = new DataFormat("dip-service/processor");
 
 		private final BorderPane pane = new BorderPane();
+		private final VBox vbox = new VBox();
 		private final Label name = new Label();
 		private final Label description = new Label();
+		private NamedGlyph namedGlyph;
+		private Glyph glyph;
 
 		public ProcessorListCell() {
 			name.setMaxWidth(Double.MAX_VALUE);
@@ -62,18 +85,15 @@ public class ProcessorsWidget extends AbstractWidget {
 			description.setMaxWidth(Double.MAX_VALUE);
 			description.setAlignment(Pos.CENTER_LEFT);
 			description.getStyleClass().add("dip-small");
-			pane.setCenter(name);
-			pane.setBottom(description);
+			pane.setCenter(vbox);
+			vbox.setPadding(new Insets(0, 0, 0, UIStrategyGUI.Stage.insets + 2));
+			vbox.getChildren().setAll(name, description);
 
 			setupDraggable();
 		}
 
 		private void setupDraggable() {
 			this.setOnDragDetected(e -> onDragDetected(e));
-//		this.setOnDragOver(e -> onDragOver(e));
-//		this.setOnDragEntered(e -> onDragEntered(e));
-//		this.setOnDragExited(e -> onDragExited(e));
-//		this.setOnDragDropped(e -> onDragDropped(e));
 			this.setOnDragDone(e -> onDragDone(e));
 		}
 
@@ -88,6 +108,8 @@ public class ProcessorsWidget extends AbstractWidget {
 			final Dragboard db = this.startDragAndDrop(TransferMode.COPY);
 			final ClipboardContent content = new ClipboardContent();
 
+			db.setDragView(getDragView());
+
 			final int index = listView.getSelectionModel().getSelectedIndex();
 			content.put(OSGI_SERVICE_PROCESSOR, listView.getItems().get(index).pid);
 			db.setContent(content);
@@ -95,33 +117,41 @@ public class ProcessorsWidget extends AbstractWidget {
 			e.consume();
 		}
 
-		private void onDragOver(DragEvent e) {
-			final Dragboard db = e.getDragboard();
-
-			// accept drop here simply to indicate dragging, even if it results in
-			// a no-op (i.e. the dragging is aborted)
-			if (db.hasContent(OSGI_SERVICE_PROCESSOR)) {
-				e.acceptTransferModes(TransferMode.COPY);
-			}
-
-			e.consume();
-		}
-
-		private void onDragEntered(DragEvent e) {
-			e.consume();
-		}
-
-		private void onDragExited(DragEvent e) {
-			e.consume();
-		}
-
-		private void onDragDropped(DragEvent e) {
-			e.setDropCompleted(true);
-			e.consume();
-		}
-
 		private void onDragDone(DragEvent e) {
 			e.consume();
+		}
+
+		private final static SnapshotParameters snapshotParams;
+		static {
+			snapshotParams = new SnapshotParameters();
+			snapshotParams.setFill(Color.TRANSPARENT);
+		}
+
+		private Image getDragView() {
+			final Glyph g = UIStrategyGUI.newGlyph(namedGlyph, Glyph.Size.HUGE);
+			g.setBackground(Color.TRANSPARENT);
+			g.setColor(Color.WHITE);
+			final Scene s = new Scene(g, Color.TRANSPARENT);
+			final WritableImage image = g.snapshot(snapshotParams, null);
+			return image;
+		}
+
+		private Glyph getGlyph(Processor p) {
+			namedGlyph = ProcessorWrapper.glyph(p);
+			glyph = UIStrategyGUI.newGlyph(namedGlyph, Glyph.Size.MEDIUM);
+			updateColor();
+			BorderPane.setAlignment(glyph, Pos.TOP_CENTER);
+			return glyph;
+		}
+
+		private final InvalidationListener glyphListener = (c) -> updateColor();
+
+		private void updateColor() {
+			if (isFocused() || isSelected()) {
+				glyph.setColor(Color.WHITE);
+			} else {
+				glyph.setColor(UIStrategyGUI.Colors.accent);
+			}
 		}
 
 		@Override
@@ -130,10 +160,18 @@ public class ProcessorsWidget extends AbstractWidget {
 			setText(null);
 			setGraphic(null);
 
+			this.focusedProperty().removeListener(glyphListener);
+			this.selectedProperty().removeListener(glyphListener);
+
 			if (!empty) {
+				this.focusedProperty().addListener(glyphListener);
+				this.selectedProperty().addListener(glyphListener);
+
 				name.setText(p.service.name());
 				description.setText(p.pid);
+				pane.setLeft(getGlyph(p.service));
 				setGraphic(pane);
+
 			}
 		}
 	}
@@ -153,6 +191,7 @@ public class ProcessorsWidget extends AbstractWidget {
 			listView.setCellFactory((ListView<ServiceMonitor.Service<Processor>> param) -> new ProcessorListCell());
 
 			listView.setItems(services.services());
+			listView.getSelectionModel().select(-1);
 			this.getChildren().addAll(listView);
 		}
 	}
