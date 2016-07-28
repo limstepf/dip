c3ae34dff76a18edecdf624f06c621d5d296ab88
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-07-28 17:16:52 +0200

    Add version (and a glyph) to the AboutDialog

--- app/src/main/java/ch/unifr/diva/dip/gui/dialogs/AboutDialog.java
+++ app/src/main/java/ch/unifr/diva/dip/gui/dialogs/AboutDialog.java
@@ -1,8 +1,12 @@
 package ch.unifr.diva.dip.gui.dialogs;
 
+import ch.unifr.diva.dip.api.ui.Glyph;
 import ch.unifr.diva.dip.core.ApplicationHandler;
 import ch.unifr.diva.dip.core.ApplicationSettings;
+import ch.unifr.diva.dip.core.ui.UIStrategyGUI;
 import ch.unifr.diva.dip.gui.layout.FormGridPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -17,6 +21,12 @@ public class AboutDialog extends AbstractDialog {
 	private final View view = new View();
 	private final Button ok;
 
+	/**
+	 * Creates a new about dialog.
+	 *
+	 * @param owner the owner/parent window of the dialog.
+	 * @param handler the application handler.
+	 */
 	public AboutDialog(Window owner, ApplicationHandler handler) {
 		super(owner);
 		setTitle(localize("about"));
@@ -27,6 +37,10 @@ public class AboutDialog extends AbstractDialog {
 		ok.setOnAction(e -> stage.hide());
 
 		view.title.setText(ApplicationSettings.applicationTitle);
+
+		// doesn't look like we get a proper version while running from the IDE
+		final String version = getClass().getPackage().getImplementationVersion();
+		view.version.setText(version == null ? "IDE-SNAPSHOT" : version);
 
 		view.javaLabel.setText(localize("system.java.version") + ":");
 		view.java.setText(
@@ -51,9 +65,13 @@ public class AboutDialog extends AbstractDialog {
 		root.setCenter(view);
 	}
 
+	/**
+	 * The inner about dialog view.
+	 */
 	private static class View extends VBox {
 
 		private final Label title = new Label();
+		private final Label version = new Label();
 		private final Label javaLabel = new Label();
 		private final Label java = new Label();
 		private final Label osLabel = new Label();
@@ -64,15 +82,30 @@ public class AboutDialog extends AbstractDialog {
 		private final Label userDir = new Label();
 		private final FormGridPane form = new FormGridPane();
 
+		/**
+		 * Creates a new inner view.
+		 */
 		public View() {
+			this.setAlignment(Pos.CENTER);
 			title.getStyleClass().add("dip-title");
+			setLeft(title);
+			setLeft(version);
+
+			final Glyph g = UIStrategyGUI.Glyphs.defaultProcessor.get(128).setColor(UIStrategyGUI.Colors.accent);
+			final double m = UIStrategyGUI.Stage.insets;
+			VBox.setMargin(g, new Insets(m, 0, m * 2, 0));
 
 			form.addRow(javaLabel, java);
 			form.addRow(osLabel, os);
 			form.addRow(appDirLabel, appDir);
 			form.addRow(userDirLabel, userDir);
 
-			this.getChildren().addAll(title, form);
+			this.getChildren().addAll(title, version, g, form);
+		}
+
+		private void setLeft(Label label) {
+			label.setMaxWidth(Double.MAX_VALUE);
+			label.setAlignment(Pos.CENTER_LEFT);
 		}
 	}
 
