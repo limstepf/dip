2f01efbe5250cbb86812cd4e6cbf442ffd377d29
Lukas Imstepf <lukas.imstepf@unifr.ch>
2016-08-25 17:19:20 +0200

    Add FxUpscalingBenchmark (NN/Bresenham)

---
+++ docs/benchmarks/limstepf-alpha/FxUpscalingBenchmark.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+JMH benchmark analysis and plots:
+Resampling (upscaling) benchmark
+"""
+import string
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+# options
+benchmark_csvfile = 'FxUpscalingBenchmark-20160825_171254.csv'
+benchmark_prefix = 'ch.unifr.diva.dip.benchmarks.FxUpscalingBenchmark.'
+
+plt_figsize = (9,9)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+# parse file
+df = pd.read_csv(
+    benchmark_csvfile,
+    delimiter=',',
+    header=0
+);
+
+#remove namespace from benchmark names
+df['Benchmark'] = df['Benchmark'].apply(lambda x: string.replace(x, benchmark_prefix, ''))
+
+# print table
+display(df)
+
+fig = plt.figure()
+ax = pd.pivot_table(df, values='Score', columns='Benchmark', index='Param: scale').plot(
+    figsize=plt_figsize, 
+    logy=True, logx=True,
+    style='x-'
+)
+
+ax.set_xlabel('scale factor')
+ax.set_ylabel('avg. ms/op')
+
+ax.legend( # legend next to/ouside of plot
+    loc='upper left', 
+    bbox_to_anchor=(1.025, 1),
+    borderaxespad=0,
+    ncol=1
+) 
