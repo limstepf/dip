e10f504852d1caadb44d27993770fa9fbc7c92f2
Lukas Imstepf <lukas.imstepf@unifr.ch>
2017-01-08 07:49:45 +0100

    Add PresetData and DipData

---
+++ app/src/main/java/ch/unifr/diva/dip/core/model/DipData.java
@@ -0,0 +1,208 @@
+package ch.unifr.diva.dip.core.model;
+
+import ch.unifr.diva.dip.api.utils.XmlUtils;
+import ch.unifr.diva.dip.utils.IOUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javafx.collections.ObservableList;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.osgi.framework.Version;
+import org.w3c.dom.Element;
+
+/**
+ * Dip export data. A composite storage format. This is the only format that
+ * should be used to write external/standalone XML files (other than, and
+ * outside of the usual dip savefile), and allows to save different things in
+ * the same XML file (e.g. pipelines and presets). All elements below the root
+ * are optional.
+ */
+@XmlRootElement(name = "dip-data")
+@XmlAccessorType(XmlAccessType.NONE)
+public class DipData {
+
+	/**
+	 * The root tag of the dip data XML file.
+	 */
+	final public static String ROOT_TAG_NAME = "dip-data";
+
+	/**
+	 * Preset data. May be null.
+	 */
+	@XmlElement(name = "presets")
+	public PresetData presets;
+
+	/**
+	 * Pipeline data. May be null.
+	 */
+	@XmlElement(name = "pipelines")
+	public PipelineData pipelines;
+
+	/**
+	 * Creates a new, empty DipData object.
+	 */
+	public DipData() {
+
+	}
+
+	/**
+	 * Checks whether preset data is available.
+	 *
+	 * @return true if there are presets, false otherwise.
+	 */
+	public boolean hasPresetData() {
+		return this.presets != null;
+	}
+
+	/**
+	 * Savely returns the preset data. Returns a new, empty preset data object
+	 * if no presets are available.
+	 *
+	 * @return the preset data.
+	 */
+	public PresetData getPresetData() {
+		if (!hasPresetData()) {
+			return new PresetData();
+		}
+		return this.presets;
+	}
+
+	/**
+	 * Sets the preset data.
+	 *
+	 * @param presets the preset data.
+	 */
+	public void setPresetData(PresetData presets) {
+		this.presets = presets;
+	}
+
+	/**
+	 * Sets all presets of given pid and version. This replaces(!) all presets
+	 * for the same processor (pid and version) with the new set of presets.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param version the version of the processor.
+	 * @param presets the set of presets.
+	 */
+	public void setPresets(String pid, Version version, ObservableList<PresetData.PresetItem> presets) {
+		if (this.presets == null) {
+			this.presets = new PresetData();
+		}
+		this.presets.setPresets(pid, version, presets);
+	}
+
+	/**
+	 * Checks whether pipeline data is available.
+	 *
+	 * @return true if there are pipelines, false otherwise.
+	 */
+	public boolean hasPipelineData() {
+		return this.pipelines != null;
+	}
+
+	/**
+	 * Savely returns the pipeline data. Returns a new, empty pipeline data
+	 * object if no pipelines are available.
+	 *
+	 * @return the preset data.
+	 */
+	public PipelineData getPipelineData() {
+		if (!hasPipelineData()) {
+			return new PipelineData();
+		}
+		return this.pipelines;
+	}
+
+	/**
+	 * Sets the pipeline data.
+	 *
+	 * @param pipelines the pipeline data.
+	 */
+	public void setPipelineData(PipelineData pipelines) {
+		this.pipelines = pipelines;
+	}
+
+	/**
+	 * Sets all pipelines. This replaces(!) all pipelines with the new set of
+	 * pipelines.
+	 *
+	 * @param pipelines the set of pipeline items.
+	 */
+	public void setPipelines(ObservableList<PipelineData.PipelineItem> pipelines) {
+		if (this.pipelines == null) {
+			this.pipelines = new PipelineData();
+		}
+		this.pipelines.setPipelines(pipelines);
+	}
+
+	/**
+	 * Checks whether or not a file is a dip data file.
+	 *
+	 * @param file path to the file.
+	 * @return true if it is a dip data file, false otherwise.
+	 */
+	public static boolean isDipDataFile(Path file) {
+		if (Files.exists(file)) {
+			try (InputStream is = Files.newInputStream(file)) {
+				return isDipDataStream(is);
+			} catch (IOException ex) {
+				// fall-through
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Checks whether or not the input stream is a dip data file.
+	 *
+	 * @param is the input stream.
+	 * @return true if it is a dip data file, false otherwise.
+	 */
+	public static boolean isDipDataStream(InputStream is) {
+		final Element root = IOUtils.getRootElement(is);
+		if (root == null) {
+			return false;
+		}
+		return root.getTagName().equals(DipData.ROOT_TAG_NAME);
+	}
+
+	/**
+	 * Loads or creates new dip data. Depending on whether the file exists, and
+	 * whether it is a valid dip data file, 3 cases can occur: 1) the file does
+	 * not exist, then new/empty dip data is returned, 2) the file exists and is
+	 * valid dip data, then that dip data is loaded and returned, 3) the file
+	 * exists but is not valid dip data, then an exception is thrown.
+	 *
+	 * @param file the dip data file.
+	 * @return dip data.
+	 * @throws java.io.IOException
+	 * @throws javax.xml.bind.JAXBException
+	 * @throws java.lang.ClassCastException
+	 */
+	public static DipData load(Path file) throws IOException, JAXBException, ClassCastException {
+		if (!Files.exists(file)) {
+			return new DipData();
+		}
+		try (InputStream is = Files.newInputStream(file)) {
+			return XmlUtils.unmarshal(DipData.class, is);
+		} catch (IOException | JAXBException | ClassCastException ex) {
+			throw (ex);
+		}
+	}
+
+	/**
+	 * Saves dip data.
+	 *
+	 * @param file file to store the dip data.
+	 * @throws JAXBException in case of unexpected errors during marshalling.
+	 */
+	public void save(Path file) throws JAXBException {
+		XmlUtils.marshal(this, file);
+	}
+
+}
---
+++ app/src/main/java/ch/unifr/diva/dip/core/model/PresetData.java
@@ -0,0 +1,337 @@
+package ch.unifr.diva.dip.core.model;
+
+import ch.unifr.diva.dip.api.ui.NamedGlyph;
+import ch.unifr.diva.dip.core.ui.Localizable;
+import ch.unifr.diva.dip.glyphs.MaterialDesignIcons;
+import ch.unifr.diva.dip.gui.pe.DataItemListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.osgi.framework.Version;
+
+/**
+ * Processor preset data. Used to store parameters of processors.
+ */
+@XmlRootElement(name = "presets")
+@XmlAccessorType(XmlAccessType.NONE)
+public class PresetData {
+
+	/**
+	 * List of presets.
+	 */
+	@XmlElement(name = "preset")
+	public List<Preset> list = new ArrayList<>();
+
+	/**
+	 * Creates a new, empty preset data object.
+	 */
+	public PresetData() {
+
+	}
+
+	/**
+	 * Returns a list of available versions (with some presets) for the
+	 * processor with the given pid.
+	 *
+	 * @param pid the pid of the processor.
+	 * @return a list of available versions (with some presets).
+	 */
+	public ObservableList<String> getVersions(String pid) {
+		return getVersions(pid, null);
+	}
+
+	/**
+	 * Returns a list of available versions (with some presets) for the
+	 * processor with the given pid.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param requiredVersion a version to be included in the returned list, no
+	 * matter what, or null.
+	 * @return a list of available versions (with some presets).
+	 */
+	public ObservableList<String> getVersions(String pid, String requiredVersion) {
+		final ObservableList<String> versions = FXCollections.observableArrayList();
+		for (Preset p : this.list) {
+			if (p.pid.equals(pid)) {
+				if (!versions.contains(p.version)) {
+					versions.add(p.version);
+				}
+			}
+		}
+		if (requiredVersion != null && !versions.contains(requiredVersion)) {
+			versions.add(requiredVersion);
+		}
+		versions.sort(String.CASE_INSENSITIVE_ORDER);
+		return versions;
+	}
+
+	/**
+	 * Returns the truncanated preset version. Presets ignore the micro-part and
+	 * qualifier of the version.
+	 *
+	 * @param version version of the service/processor.
+	 * @return truncanated preset version.
+	 */
+	public static String toPresetVersion(Version version) {
+		return String.format("%d.%d", version.getMajor(), version.getMinor());
+	}
+
+	/**
+	 * Adds a preset to the preset data.
+	 *
+	 * @param preset the new preset.
+	 */
+	public void addPreset(Preset preset) {
+		this.list.add(preset);
+	}
+
+	/**
+	 * Adds a preset to the preset data.
+	 *
+	 * @param item the new preset item.
+	 */
+	public void addPreset(PresetItem item) {
+		addPreset(item.toPresetData());
+	}
+
+	/**
+	 * Returns a list of presets for the processor with given pid and version.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param version the version of the processor.
+	 * @return a list of presets.
+	 */
+	public ObservableList<PresetItem> getPresets(String pid, Version version) {
+		return getPresets(pid, toPresetVersion(version));
+	}
+
+	/**
+	 * Returns a list of presets for the processor with given pid and version.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param version the version of the processor.
+	 * @return a list of presets.
+	 */
+	public ObservableList<PresetItem> getPresets(String pid, String version) {
+		final ObservableList<PresetItem> presets = FXCollections.observableArrayList();
+		for (Preset p : this.list) {
+			if (!p.pid.equals(pid) || !p.version.equals(version)) {
+				continue;
+			}
+			presets.add(new PresetItem(p));
+		}
+		return presets;
+	}
+
+	/**
+	 * Sets all presets of given pid and version. This replaces(!) all presets
+	 * for the same processor (pid and version) with the new set of presets.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param version the version of the processor.
+	 * @param presets the set of presets.
+	 */
+	public void setPresets(String pid, Version version, ObservableList<PresetItem> presets) {
+		setPresets(pid, toPresetVersion(version), presets);
+	}
+
+	/**
+	 * Sets all presets of given pid and version. This replaces(!) all presets
+	 * for the same processor (pid and version) with the new set of presets.
+	 *
+	 * @param pid the pid of the processor.
+	 * @param version the version of the processor.
+	 * @param presets the set of presets.
+	 */
+	public void setPresets(String pid, String version, ObservableList<PresetItem> presets) {
+		// remove all modified set of presets
+		final ArrayList<Preset> deprecated = new ArrayList<>();
+		for (Preset p : this.list) {
+			if (p.pid.equals(pid) && p.version.equals(version)) {
+				deprecated.add(p);
+			}
+		}
+		this.list.removeAll(deprecated);
+
+		// (re-)add modified set of presets
+		for (PresetItem item : presets) {
+			addPreset(item);
+		}
+	}
+
+	/**
+	 * A preset object.
+	 */
+	public static class Preset {
+
+		/**
+		 * PID of the processor.
+		 */
+		@XmlAttribute
+		public String pid;
+
+		/**
+		 * Version of the processor. This is a version string truncanated to
+		 * only include the major and minor, but not the micro or qualifier part
+		 * of the full semantic version (e.g. "1.0").
+		 */
+		@XmlAttribute
+		public String version;
+
+		/**
+		 * Name of the preset.
+		 */
+		@XmlAttribute
+		public String name;
+
+		/**
+		 * Parameter values of the preset.
+		 */
+		@XmlElement
+		public Map<String, Object> parameters = new HashMap<>();
+
+		/**
+		 * Empty constructor (needed for JAXB).
+		 */
+		public Preset() {
+		}
+
+		/**
+		 * Creates a new preset from the given processor.
+		 *
+		 * @param name the name of the preset.
+		 * @param wrapper the processor to copy its parameters from.
+		 */
+		public Preset(String name, ProcessorWrapper wrapper) {
+			this(
+					wrapper.pid,
+					toPresetVersion(wrapper.version),
+					name,
+					wrapper.parameters() // no deep copy needed
+			);
+		}
+
+		/**
+		 * Creates a new preset.
+		 *
+		 * @param pid PID of the processor.
+		 * @param version version of the processor.
+		 * @param name name of the preset.
+		 * @param parameters map of parameter values.
+		 */
+		public Preset(String pid, String version, String name, Map<String, Object> parameters) {
+			this.pid = pid;
+			this.version = version;
+			this.name = name;
+			this.parameters = parameters;
+		}
+
+	}
+
+	/**
+	 * A preset item. A list item pointing to an existing preset data object, or
+	 * to a processor in order to create a new preset.
+	 */
+	public static class PresetItem implements Localizable, DataItemListView.DataItem {
+
+		final private PresetData.Preset preset; // existing preset, or
+		final private ProcessorWrapper wrapper; // new preset
+		final private StringProperty nameProperty;
+		final private ObjectProperty<NamedGlyph> glyphProperty;
+
+		/**
+		 * Creates a new preset item for a processor.
+		 *
+		 * @param wrapper the processor.
+		 */
+		public PresetItem(ProcessorWrapper wrapper) {
+			this(null, wrapper);
+		}
+
+		/**
+		 * Creates a new preset item for a preset data object.
+		 *
+		 * @param preset the preset.
+		 */
+		public PresetItem(PresetData.Preset preset) {
+			this(preset, null);
+		}
+
+		/**
+		 * Creates a new preset item.
+		 *
+		 * @param preset the preset, or null.
+		 * @param wrapper the processor, or null.
+		 */
+		private PresetItem(PresetData.Preset preset, ProcessorWrapper wrapper) {
+			this.preset = preset;
+			this.wrapper = wrapper;
+			this.nameProperty = new SimpleStringProperty();
+			this.glyphProperty = new SimpleObjectProperty();
+			if (isNewItem()) {
+				this.glyphProperty.set(MaterialDesignIcons.FLOPPY);
+				this.nameProperty.setValue(localize("preset.new"));
+			} else {
+				this.nameProperty.setValue(this.preset.name);
+			}
+		}
+
+		/**
+		 * Returns the name property of the preset item.
+		 *
+		 * @return the name property of the preset item.
+		 */
+		@Override
+		public StringProperty nameProperty() {
+			return nameProperty;
+		}
+
+		/**
+		 * Checks whether this preset item refers to a new preset (to be
+		 * exported/saved), or to an existing one.
+		 *
+		 * @return true if this item refers to a new preset, false otherwise.
+		 */
+		final public boolean isNewItem() {
+			return this.wrapper != null;
+		}
+
+		@Override
+		public ObjectProperty<NamedGlyph> glyphProperty() {
+			return this.glyphProperty;
+		}
+
+		/**
+		 * Returns the preset data object.
+		 *
+		 * @return the preset data object.
+		 */
+		public Preset toPresetData() {
+			if (this.preset != null) {
+				this.preset.name = this.nameProperty.get();
+				return this.preset;
+			}
+			if (this.wrapper != null) {
+				return new Preset(
+						this.nameProperty.get(),
+						this.wrapper
+				);
+			}
+			return null;
+		}
+
+	}
+
+}
