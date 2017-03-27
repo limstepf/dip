ca838bfabf8665f921d422a533d168d524aba17e
Lukas Imstepf <lukas.imstepf@unifr.ch>
2017-03-27 04:06:15 +0200

    Add a simple move gesture

---
+++ api/src/main/java/ch/unifr/diva/dip/api/tools/MoveGesture.java
@@ -0,0 +1,38 @@
+package ch.unifr.diva.dip.api.tools;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * A move(-ement) gesture.
+ */
+public class MoveGesture extends GestureBase<MouseEvent> {
+
+	/**
+	 * Creates a new move gesture (without entered/exited handlers).
+	 *
+	 * @param onMoved the moved (and dragged) handler, or null.
+	 */
+	public MoveGesture(EventHandler<MouseEvent> onMoved) {
+		this(null, onMoved, null);
+	}
+
+	/**
+	 * Creates a new move gesture (with entered/exited handlers).
+	 *
+	 * @param onEntered the entered handler, or null.
+	 * @param onMoved the moved (and dragged) handler, or null.
+	 * @param onExited the exited handler.
+	 */
+	public MoveGesture(EventHandler<MouseEvent> onEntered, EventHandler<MouseEvent> onMoved, EventHandler<MouseEvent> onExited) {
+		if (onMoved != null) {
+			eventHandlers().add(new GestureEventHandler(
+					MouseEvent.MOUSE_DRAGGED,
+					onMoved
+			));
+		}
+
+		addMouseEventHandlers(onEntered, onMoved, onExited);
+	}
+
+}
