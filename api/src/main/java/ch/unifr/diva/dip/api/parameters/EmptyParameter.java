224399fa1870f0fbe53490b43890bd2e4f6bcff6
Lukas Imstepf <lukas.imstepf@unifr.ch>
2017-08-11 06:21:58 +0200

    Add minWidth/-Height to the view of EmptyParameter

--- api/src/main/java/ch/unifr/diva/dip/api/parameters/EmptyParameter.java
+++ api/src/main/java/ch/unifr/diva/dip/api/parameters/EmptyParameter.java
@@ -9,14 +9,49 @@ import javafx.scene.layout.Region;
  */
 public class EmptyParameter implements Parameter<Void> {
 
-	private Parameter.View view;
+	private EmptyView view;
 	protected boolean isHidden;
+	protected double minWidth;
+	protected double minHeight;
 
 	/**
 	 * Creates an empty parameter.
 	 */
 	public EmptyParameter() {
+		this(-1, -1);
+	}
 
+	/**
+	 * Creates an empty parameter.
+	 *
+	 * @param minWidth the minimum width of the empty parameter's view/region.
+	 * @param minHeight the minimum height of the empty parameter's view/region.
+	 */
+	public EmptyParameter(double minWidth, double minHeight) {
+		this.minWidth = minWidth;
+		this.minHeight = minHeight;
+	}
+
+	/**
+	 * Sets a minimum width. By default the view/region of the empty parameter
+	 * doesn't occupy any space. This allows the empty parameter to act as a
+	 * spacer.
+	 *
+	 * @param minWidth the minimum width of the empty parameter's view/region.
+	 */
+	public void setMinWidth(double minWidth) {
+		this.minWidth = minWidth;
+	}
+
+	/**
+	 * Sets a minimum height. By default the view/region of the empty parameter
+	 * doesn't occupy any space. This allows the empty parameter to act as a
+	 * spacer.
+	 *
+	 * @param minHeight the minimum height of the empty parameter's view/region.
+	 */
+	public void setMinHeight(double minHeight) {
+		this.minHeight = minHeight;
 	}
 
 	@Override
@@ -30,8 +65,11 @@ public class EmptyParameter implements Parameter<Void> {
 	@Override
 	public View view() {
 		if (view == null) {
-			this.view = new EmptyView();
-			this.view.setHide(this.isHidden);
+			view = new EmptyView();
+			if (minWidth > 0) {
+				view.setMinWidth(minWidth);
+			}
+			view.setHide(this.isHidden);
 		}
 		return view;
 	}
@@ -43,10 +81,20 @@ public class EmptyParameter implements Parameter<Void> {
 
 		private final Region region = new Region();
 
+		/**
+		 * Sets the minimum width.
+		 *
+		 * @param minWidth the minimum width.
+		 */
+		public void setMinWidth(double minWidth) {
+			region.setMinWidth(minWidth);
+		}
+
 		@Override
 		public Node node() {
 			return region;
 		}
+
 	}
 
 }
